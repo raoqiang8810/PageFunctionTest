f74c7d2c26ccf91344c680fa0fc46bd09e52f7c4
raoqiang8810 <105692112+raoqiang8810@users.noreply.github.com>
2022-05-22 19:01:09 +0800

    Update _middleware.js

--- functions/_middleware.js
+++ functions/_middleware.js
@@ -10,6 +10,6 @@ export async function onRequest(context) {
     } = context;
 
     let url = new URL(request.url);
-    url.hostname = "usa.wuyouss.com";//该成自己的域名
+    url.hostname = "usavpn.wuyouss.com";//该成自己的域名
     return fetch(new Request(url, request))
 }
